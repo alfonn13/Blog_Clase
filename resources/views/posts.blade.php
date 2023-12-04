e895a9436f3a64587f667169801a8d2ea3fa8635
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2023-12-04 13:24:14 +0100

    Update

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
+        necessitatibus recusandae cumque quidem culpa dolorum, deserunt sit
+        quisquam voluptatibus quasi? Nihil sequi debitis tempora quae sapiente
+        reiciendis velit voluptatem? Quo! Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Quibusdam itaque odit tempore saepe dignissimos numquam
+        nisi cum optio sit ipsam maiores deserunt minima dolore ad aspernatur ullam
+        dolores, modi totam.</p>
+    </article>
+    
+    <article>
+        <h1>My Second Post</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
+        necessitatibus recusandae cumque quidem culpa dolorum, deserunt sit
+        quisquam voluptatibus quasi? Nihil sequi debitis tempora quae sapiente
+        reiciendis velit voluptatem? Quo! Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Quibusdam itaque odit tempore saepe dignissimos numquam
+        nisi cum optio sit ipsam maiores deserunt minima dolore ad aspernatur ullam
+        dolores, modi totam.</p>
+    </article>   
+
+    <article>
+        <h1>My Third Post</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
+        necessitatibus recusandae cumque quidem culpa dolorum, deserunt sit
+        quisquam voluptatibus quasi? Nihil sequi debitis tempora quae sapiente
+        reiciendis velit voluptatem? Quo! Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Quibusdam itaque odit tempore saepe dignissimos numquam
+        nisi cum optio sit ipsam maiores deserunt minima dolore ad aspernatur ullam
+        dolores, modi totam.</p>
+    </article>
+   
+    <script src="./app.js"></script>
+</body>
+
+</html>
